f48c0ebed74ebff43cfc71451a526dcb09279c9e
Frederick <fjdevel@gmail.com>
2019-12-05 10:19:02 -0600

    agregando template

--- portafolio/src/Components/NavBar.js
+++ portafolio/src/Components/NavBar.js
@@ -1,32 +1,57 @@
 import React, { Component } from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { Link } from 'react-router-dom';
 
-import logo from "../static/img/spooky.svg"
-
+import img1 from "../static/img/images/Group2.svg";
 export default class NavBar extends Component {
   render() {
     return (
-      <div>
-        <Navbar >
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={logo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            Mi portafolio
-          </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Link className="nav-link"  to="/">Home</Link>
-            <Link className="nav-link"  to="/projects">Projects</Link>
-            <Link className="nav-link"  to="/contact">Contact</Link>
-          </Nav>
-        </Navbar>
-      </div>
+      <header id="header-section">
+        <nav class="navbar navbar-expand-lg pl-3 pl-sm-0" id="navbar">
+        <div class="container">
+          <div class="navbar-brand-wrapper d-flex w-100">
+            <img src={img1} alt=""></img>
+            <button
+              class="navbar-toggler ml-auto"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="mdi mdi-menu navbar-toggler-icon"></span>
+            </button>
+          </div>
+          <div class="collapse navbar-collapse navbar-menu-wrapper" id="navbarSupportedContent">
+          <ul class="navbar-nav align-items-lg-center align-items-start ml-auto">
+          <li class="d-flex align-items-center justify-content-between pl-4 pl-lg-0">
+            <div class="navbar-collapse-logo">
+              <img src={img1}alt=""/>
+            </div>
+            <button class="navbar-toggler close-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="mdi mdi-close navbar-toggler-icon pl-5"></span>
+            </button>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#header-section">Inicio<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#features-section">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#digital-marketing-section">Sucursales</a>  
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#feedback-section">Opiniones</a>
+          </li>
+          <li class="nav-item btn-contact-us pl-4 pl-lg-0">
+            <button class="btn btn-info" data-toggle="modal" data-target="#exampleModal">Pedidos</button>
+          </li>
+          </ul>
+          </div>
+        </div>
+      </nav>
+      </header>
+      
     );
   }
 }
--- portafolio/src/Views/inicio.js
+++ portafolio/src/Views/inicio.js
@@ -1,7 +1,62 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import img2 from "../static/img/images/Group171.svg"
+import img3 from "../static/img/images/Group12.svg"
 export default class Inicio extends Component {
-    render(){
-        return(<h1>Hola mundo</h1>)
-    }
+  render() {
+    return (
+      <div>
+        <div class="banner">
+          <div class="container">
+            <h1 class="font-weight-semibold">
+              Las mejores pupusas de El Salvador
+            </h1>
+            <h6 class="font-weight-normal text-muted pb-3">
+              Mejores que las de olocuilta
+            </h6>
+            <div>
+              <button class="btn btn-opacity-light mr-1">
+                Mira nuestro menu
+              </button>
+              <button class="btn btn-opacity-success ml-1">
+                Has pedidos por whatsapp
+              </button>
+            </div>
+            <img src={img2} alt="" class="img-fluid mt-5" />
+            <section class="features-overview" id="features-section" >
+            <div class="content-header">
+            <h2>How does it works</h2>
+            <h6 class="section-subtitle text-muted">One theme that serves as an easy-to-use operational toolkit<br/>that meets customer's needs.</h6>
+            </div>
+            <div class="d-md-flex justify-content-between">
+          <div class="grid-margin d-flex justify-content-start">
+            <div class="features-width">
+              <img src={img3}alt="" class="img-icons"/>
+              <h5 class="py-3">Speed<br/>Optimisation</h5>
+              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
+              <a href="#"><p class="readmore-link">Readmore</p></a>  
+            </div>
+          </div>
+          <div class="grid-margin d-flex justify-content-center">
+            <div class="features-width">
+              <img src="images/Group7.svg" alt="" class="img-icons"/>
+              <h5 class="py-3">SEO and<br/>Backlinks</h5>
+              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
+              <a href="#"><p class="readmore-link">Readmore</p></a>
+            </div>
+          </div>
+          <div class="grid-margin d-flex justify-content-end">
+            <div class="features-width">
+              <img src="images/Group5.svg" alt="" class="img-icons"/>
+              <h5 class="py-3">Content<br/>Marketing</h5>
+              <p class="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
+              <a href="#"><p class="readmore-link">Readmore</p></a>
+            </div>
+          </div>
+        </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
