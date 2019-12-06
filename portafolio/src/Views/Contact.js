71034060919db3de57b40b6e31d5d4d83b01efe3
Frederick <fjdevel@gmail.com>
2019-12-06 09:19:26 -0600

    agregando formulario base

--- portafolio/src/Views/Contact.js
+++ portafolio/src/Views/Contact.js
@@ -1,7 +1,34 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Contact extends Component {
-    render(){
-        return(<h1>Contact</h1>)
-    }
+  render() {
+    return (
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <form>
+              <div class="form-group">
+                <label for="formGroupExampleInput">Example label</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Example input"
+                />
+              </div>
+              <div class="form-group">
+                <label for="formGroupExampleInput2">Another label</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Another input"
+                />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
